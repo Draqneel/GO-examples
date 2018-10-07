bf6283cae4ef2efe42647c10361876284089b7f1
Draqneel <marabucavi@gmail.com>
2018-10-07 17:43:39 +0300

    add Scrum task (ACM timus) with specification

---
+++ Scram/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/*
+ Specification: http://acm.timus.ru/problem.aspx?space=1&num=2095&locale=en
+*/
+const maxvalue = 1000000000
+
+func main() {
+	var (
+	 fistBoard int
+	 secondBoard int
+	 checkDay int = 0
+	 result int = 0
+	)
+	fmt.Println("Add 1st value")
+	fmt.Scan(&fistBoard)
+	fmt.Println("Add 2nd value")
+	fmt.Scan(&secondBoard)
+	if (fistBoard < 1 || fistBoard > maxvalue) && (secondBoard < 1 || secondBoard > maxvalue) && (fistBoard > secondBoard) {
+		fmt.Println("Incorrect input")
+		return
+	}
+
+	for checkDay < maxvalue {
+		if checkDay > secondBoard {
+			break
+		}
+		checkDay += 1
+		if (checkDay >= fistBoard)&&(checkDay <= secondBoard){
+			result++
+		}
+		checkDay += 2
+		if (checkDay >= fistBoard)&&(checkDay <= secondBoard){
+			result++
+		}
+		checkDay += 4
+		if (checkDay >= fistBoard)&&(checkDay <= secondBoard){
+			result++
+		}
+		checkDay += 3
+	}
+
+	fmt.Println("result", result)
+}
