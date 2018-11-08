5e71478b3e928cafbcb04f0e8c9099f2cee3c350
Draqneel <marabucavi@gmail.com>
2018-11-08 19:29:07 +0300

    add go-routine example

---
+++ LibExample/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type MyObj struct {
+	dataMutex *sync.Mutex
+	data []int
+	count int
+}
+
+func (obj *MyObj) addData(value int)  {
+	obj.dataMutex.Lock()
+	defer obj.dataMutex.Unlock()
+	obj.data[obj.count] = value
+	obj.count ++
+}
+
+func main()  {
+	obj := MyObj{
+		data: make([]int, 5),
+		dataMutex: new(sync.Mutex),
+		count: 0,
+	}
+	obj.addData(10)
+	obj.addData(50)
+	obj.addData(100)
+	fmt.Println(obj.data)
+	//
+
+	wg := new(sync.WaitGroup)
+	for i:=0;i<10;i++ {
+		go Print("Piece", wg)
+	}
+	wg.Wait()
+}
+
+func Print(word string, wg *sync.WaitGroup)  {
+	wg.Add(1)
+	defer wg.Done()
+	fmt.Println(word)
+}
+
