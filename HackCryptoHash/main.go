0f702d74e09d37a3b21c15c2eed9f8099228bb79
Draqneel <marabucavi@gmail.com>
2019-01-22 00:07:30 +0300

    add code refactor in HackCryptoHash prj

--- HackCryptoHash/main.go
+++ HackCryptoHash/main.go
@@ -12,24 +12,23 @@ import (
 )
 
 //READ DICTIONARY.TXT !!!
-func main(){
+func main() {
 	if len(os.Args) < 4 {
 		get_error("OS args less then 4")
 	}
-	check_mode(os.Args[1])
-	hack_password(os.Args[1], os.Args[2], os.Args[3])
+	checkMode(os.Args[1])
+	hackPassword(os.Args[1], os.Args[2], os.Args[3])
 }
 
-func check_mode(mode string){
-	if mode != "md5" && mode != "sha256" && mode != "sha512"{
+func checkMode(mode string) {
+	if mode != "md5" && mode != "sha256" && mode != "sha512" {
 		get_error("crypto-hash func is not found")
 	}
 }
 
-
-func hack_password(mode string, dictionary,static_hash string)  {
+func hackPassword(mode string, dictionary, static_hash string) {
 	file, err := os.Open(dictionary)
-	check_error(err)
+	checkError(err)
 	defer file.Close()
 
 	var reader *bufio.Reader = bufio.NewReader(file)
@@ -38,7 +37,7 @@ func hack_password(mode string, dictionary,static_hash string)  {
 	fmt.Println("----------START----------")
 	for {
 		password, _ = reader.ReadString('\n')
-		password = strings.Replace(password,"\n","",-1)
+		password = strings.Replace(password, "\n", "", -1)
 		dynamic_hash = encrypt(mode, password)
 		if static_hash == dynamic_hash {
 			fmt.Println("----------(!!!)---------")
@@ -67,13 +66,13 @@ func encrypt(crypt string, text string) string {
 	}
 }
 
-func check_error(err error)  {
-	if (err != nil) {
+func checkError(err error) {
+	if err != nil {
 		get_error(err.Error())
 	}
 }
 
-func get_error(err string)  {
-	fmt.Println("error:",err)
+func get_error(err string) {
+	fmt.Println("error:", err)
 	os.Exit(1)
 }
