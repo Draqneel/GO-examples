a71814f13cf1f0fa2066e763af0596e1b8337b69
Draqneel <marabucavi@gmail.com>
2019-12-11 18:45:54 +0300

    add web scrapper project

---
+++ test_grabber/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://coinmarketcap.com/ru/coins/views/all/")
+	errProcessing(err)
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	errProcessing(err)
+	var links []string
+	var titles []string
+	var reductions []string
+
+
+
+	doc.Find(".kQmhAn td .eTVhdN").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		link, _ := s.Find("a").Attr("href")
+		title := s.Find("a").Text()
+
+		links = append(links, link)
+		titles = append(titles, title)
+	})
+
+	doc.Find(".kQmhAn td .dWPDaQ").Each(func(i int, s *goquery.Selection) {
+		reductions = append(reductions, s.Text())
+	})
+
+	for i, link := range links {
+		fmt.Printf("Review %d: %s - %s (%s)\n", i, "https://coinmarketcap.com/" + link, titles[i], reductions[i])
+	}
+}
+
+func errProcessing(err error)  {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
